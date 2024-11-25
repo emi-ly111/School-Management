f7735eb106be90ad38f7e3da22866878de326957
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2024-11-25 19:34:45 -0300

    refactor(password)

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE poo_at2;
+CREATE DATABASE school_management;
 
-\c poo_at2;
+\c school_management;
 
-CREATE TABLE estudante (
+CREATE TABLE student (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL,
     idade VARCHAR(200) NOT NULL,
@@ -16,28 +16,28 @@ CREATE TABLE professor (
     especialidade VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE curso (
+CREATE TABLE course (
     id SERIAL PRIMARY KEY,
     nome VARCHAR(255) NOT NULL UNIQUE,
     carga_horaria INT NOT NULL,
     professor_id INT REFERENCES professor(id) ON DELETE SET NULL -- Define que, caso o professor seja excluído, o campo professor_id será NULL
 );
 
-CREATE TABLE matricula_estudante (
+CREATE TABLE matricula_student (
     id SERIAL PRIMARY KEY,
-    estudante_id INT NOT NULL REFERENCES estudante(id) ON DELETE CASCADE,
-    curso_id INT NOT NULL REFERENCES curso(id) ON DELETE CASCADE,
-    UNIQUE(estudante_id, curso_id)
+    student_id INT NOT NULL REFERENCES student(id) ON DELETE CASCADE,
+    course_id INT NOT NULL REFERENCES course(id) ON DELETE CASCADE,
+    UNIQUE(student_id, course_id)
 );
 
 --
 CREATE OR REPLACE FUNCTION InsertStudents(new_nome VARCHAR(255), new_idade VARCHAR(200), new_matricula VARCHAR(20)) RETURNS VOID AS $vai$
 DECLARE
-    estudante_id INT;
+    student_id INT;
 BEGIN
-    INSERT INTO estudante (nome, idade, matricula) VALUES (new_nome, new_idade, new_matricula) RETURNING id INTO estudante_id;
+    INSERT INTO student (nome, idade, matricula) VALUES (new_nome, new_idade, new_matricula) RETURNING id INTO student_id;
 
-    RAISE NOTICE 'Inserção realizada com sucesso: % - %', estudante_id, new_matricula;
+    RAISE NOTICE 'Inserção realizada com sucesso: % - %', student_id, new_matricula;
 END;
 $vai$ LANGUAGE plpgsql;
 
@@ -56,12 +56,12 @@ $vai$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION InsertCourse(new_nome VARCHAR(255), new_carga_horaria INT, professor_id INT) RETURNS VOID AS $vai$
 DECLARE
     exist BOOLEAN;
-    nome_curso VARCHAR(255);
+    nome_course VARCHAR(255);
 BEGIN
     SELECT EXISTS(SELECT id FROM professor WHERE id = professor_id) INTO exist;
     IF exist THEN
-        INSERT INTO curso (nome, carga_horaria, professor_id) VALUES (new_nome, new_carga_horaria, professor_id) RETURNING nome INTO nome_curso;
-        RAISE NOTICE 'Inserção realizada com sucesso: % - %', nome_curso, new_carga_horaria;
+        INSERT INTO course (nome, carga_horaria, professor_id) VALUES (new_nome, new_carga_horaria, professor_id) RETURNING nome INTO nome_course;
+        RAISE NOTICE 'Inserção realizada com sucesso: % - %', nome_course, new_carga_horaria;
     ELSE
         RAISE NOTICE 'Professor não encontrado: %', professor_id;
     END IF;
@@ -70,25 +70,25 @@ $vai$ LANGUAGE plpgsql;
 
 --
 CREATE OR REPLACE FUNCTION InsertMatricula(
-    p_estudante_id INT,
-    p_curso_id INT
+    p_student_id INT,
+    p_course_id INT
 ) RETURNS TEXT AS $$
 DECLARE
-    estudante_existe BOOLEAN;
-    curso_existe BOOLEAN;
+    student_existe BOOLEAN;
+    course_existe BOOLEAN;
 BEGIN
-    SELECT EXISTS(SELECT 1 FROM estudante WHERE id = p_estudante_id) INTO estudante_existe;
-    IF NOT estudante_existe THEN
-        RETURN 'Estudante não encontrado.';
+    SELECT EXISTS(SELECT 1 FROM student WHERE id = p_student_id) INTO student_existe;
+    IF NOT student_existe THEN
+        RETURN 'student não encontrado.';
     END IF;
 
-    SELECT EXISTS(SELECT 1 FROM curso WHERE id = p_curso_id) INTO curso_existe;
-    IF NOT curso_existe THEN
-        RETURN 'Curso não encontrado.';
+    SELECT EXISTS(SELECT 1 FROM course WHERE id = p_course_id) INTO course_existe;
+    IF NOT course_existe THEN
+        RETURN 'course não encontrado.';
     END IF;
 
-    INSERT INTO matricula_estudante (estudante_id, curso_id)
-    VALUES (p_estudante_id, p_curso_id);
+    INSERT INTO matricula_student (student_id, course_id)
+    VALUES (p_student_id, p_course_id);
 
     RETURN 'Matrícula criada com sucesso.';
 END;
@@ -97,7 +97,7 @@ $$ LANGUAGE plpgsql;
 --
 CREATE OR REPLACE FUNCTION DeleteStudents(student_id INT) RETURNS VOID AS $BORA$
 BEGIN
-    DELETE FROM estudante WHERE id = student_id;
+    DELETE FROM student WHERE id = student_id;
 END;
 $BORA$ LANGUAGE plpgsql;
 
@@ -108,12 +108,12 @@ CREATE OR REPLACE FUNCTION DeleteMatricula(
 DECLARE
     matricula_existe BOOLEAN;
 BEGIN
-    SELECT EXISTS(SELECT 1 FROM matricula_estudante WHERE id = p_matricula_id) INTO matricula_existe;
+    SELECT EXISTS(SELECT 1 FROM matricula_student WHERE id = p_matricula_id) INTO matricula_existe;
     IF NOT matricula_existe THEN
         RETURN 'Matrícula não encontrada.';
     END IF;
 
-    DELETE FROM matricula_estudante WHERE id = p_matricula_id;
+    DELETE FROM matricula_student WHERE id = p_matricula_id;
 
     RETURN 'Matrícula deletada com sucesso.';
 END;
@@ -129,7 +129,7 @@ $BORA$ LANGUAGE plpgsql;
 --
 CREATE OR REPLACE FUNCTION DeleteCourse(course_id INT) RETURNS VOID AS $BORA$
 BEGIN
-    DELETE FROM curso WHERE id = course_id;
+    DELETE FROM course WHERE id = course_id;
 END;
 $BORA$ LANGUAGE plpgsql;
 
@@ -139,18 +139,18 @@ DECLARE
     exist BOOLEAN;
     exist_teacher BOOLEAN;
 BEGIN
-    SELECT EXISTS(SELECT id FROM curso WHERE id = course_id) INTO exist;
+    SELECT EXISTS(SELECT id FROM course WHERE id = course_id) INTO exist;
     IF exist THEN
         SELECT EXISTS(SELECT id FROM professor WHERE id = new_professor_id) INTO exist_teacher;
         IF exist_teacher THEN
-            UPDATE curso
+            UPDATE course
             SET nome = new_nome, carga_horaria = new_carga_horaria, professor_id = new_professor_id
             WHERE id = course_id;
         ELSE
             RAISE NOTICE 'O professor com o id=% não existe', new_professor_id;
         END IF;
     ELSE
-        RAISE NOTICE 'O curso com o id % não existe', course_id;
+        RAISE NOTICE 'O course com o id % não existe', course_id;
     END IF;
 END;
 $ACABA$ LANGUAGE plpgsql;
@@ -158,24 +158,24 @@ $ACABA$ LANGUAGE plpgsql;
 --
 CREATE OR REPLACE FUNCTION UpdateMatricula(
     p_matricula_id INT,
-    p_novo_curso_id INT
+    p_novo_course_id INT
 ) RETURNS TEXT AS $$
 DECLARE
     matricula_existe BOOLEAN;
-    curso_existe BOOLEAN;
+    course_existe BOOLEAN;
 BEGIN
-    SELECT EXISTS(SELECT 1 FROM matricula_estudante WHERE id = p_matricula_id) INTO matricula_existe;
+    SELECT EXISTS(SELECT 1 FROM matricula_student WHERE id = p_matricula_id) INTO matricula_existe;
     IF NOT matricula_existe THEN
         RETURN 'Matrícula não encontrada.';
     END IF;
 
-    SELECT EXISTS(SELECT 1 FROM curso WHERE id = p_novo_curso_id) INTO curso_existe;
-    IF NOT curso_existe THEN
-        RETURN 'Novo curso não encontrado.';
+    SELECT EXISTS(SELECT 1 FROM course WHERE id = p_novo_course_id) INTO course_existe;
+    IF NOT course_existe THEN
+        RETURN 'Novo course não encontrado.';
     END IF;
 
-    UPDATE matricula_estudante
-    SET curso_id = p_novo_curso_id
+    UPDATE matricula_student
+    SET course_id = p_novo_course_id
     WHERE id = p_matricula_id;
 
     RETURN 'Matrícula atualizada com sucesso.';
@@ -204,9 +204,9 @@ CREATE OR REPLACE FUNCTION UpdateStudent(student_id INT, new_nome VARCHAR(255), 
 DECLARE
     exist BOOLEAN;
 BEGIN
-    SELECT EXISTS(SELECT id FROM estudante WHERE id = student_id) INTO exist;
+    SELECT EXISTS(SELECT id FROM student WHERE id = student_id) INTO exist;
     IF exist THEN
-        UPDATE estudante
+        UPDATE student
         SET idade = new_idade, nome = new_nome, matricula = new_matricula
         WHERE id = student_id;
         RAISE NOTICE 'Registro atualizado com sucesso.';
