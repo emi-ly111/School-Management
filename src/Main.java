1cbd947e3b3efecdfbbc0c78d0ca133363196a3c
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2024-11-25 21:14:22 -0300

    feat(db)

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,12 @@ import src.views.*;
 
 public class Main {
   public static void main(String[] args) {
+    try {
+      Class.forName("org.postgresql.Driver");
+    } catch (ClassNotFoundException e) {      
+      e.printStackTrace();
+    }
+
     MainView mainPage = new MainView();
 
     mainPage.setVisible(true);
--- src/data/DataBase.java
+++ src/data/DataBase.java
@@ -5,11 +5,15 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBase {
-    private static final String URL = "jdbc:postgresql://localhost:5432/school";
+    private static final String URL = "jdbc:postgresql://localhost:5432/school_management";
     private static final String USER = "admin";
     private static final String PASSWORD = "admin";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar ao banco de dados: " + e.getMessage(), e);
+        }
     }
 }
